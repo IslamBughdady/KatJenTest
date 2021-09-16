822edd269e2f20c1e43780b5cc32940fbf653039
Islam Bughdady <ibughdady@gmail.com>
2021-09-16 11:01:30 +0200

    test yarab

---
+++ Test Suites/Create New User.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Create New User</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c8379784-1553-477b-9db1-1cf9a6b70d3c</testSuiteGuid>
+   <testCaseLink>
+      <guid>add293f7-35ca-4bae-abc1-673b506da3db</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Users/Create New User</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
